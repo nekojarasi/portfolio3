9e783b24f06cc5638e74cdf47a2765198c7c7b7e
nekojarasi <46546907+nekojarasi@users.noreply.github.com>
2019-02-09 10:52:35 +0900

    Add files via upload

---
+++ いろは:和菓子店サンプルページ/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#top').bgSwitcher({
+      images: ['images/niwa.jpg', 'images/take.jpg', 'images/momiji.jpg', 'images/kasa.jpg'],
+      interval: 5000,
+      loop: true,
+      shufle: false,
+      effect: 'slide',
+      duration: 2000,
+      easing: 'linear'
+    })
+    $('#home').on('mouseover', function(){
+        $(this).attr('src', 'images/home2.png');
+    });
+    $('#home').on('mouseout', function(){
+        $(this).attr('src', 'images/home.png');
+    });
+})
